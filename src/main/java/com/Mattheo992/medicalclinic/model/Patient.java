bfbea5629a228e7b7976dbc81411249ed70a4cbb
Mattheo992 <stepniak.matt@gmail.com>
2024-05-16 12:52:43 +0200

    Dodane adnotacje do klasy Patient

--- src/main/java/com/Mattheo992/medicalclinic/model/Patient.java
+++ src/main/java/com/Mattheo992/medicalclinic/model/Patient.java
@@ -1,10 +1,14 @@
 package com.Mattheo992.medicalclinic.model;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import java.time.LocalDate;
 import java.time.LocalDateTime;
 
+@AllArgsConstructor
+@NoArgsConstructor
 @Data
 public class Patient {
 private Long id;
